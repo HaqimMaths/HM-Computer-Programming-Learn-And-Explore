6dc151e8b1766c69510c4a82660f078ab23ae73d
Harish Haqim <91003888+HaqimMaths@users.noreply.github.com>
2025-01-03 23:40:40 +0800

    Kotlin Programming

---
+++ Kotlin Programming/Basics/Part One/TestVariables.kt
@@ -0,0 +1,12 @@
+/*
+  Author: Haqim Maths
+  Date: 03/01/2025
+*/
+
+fun main() {
+  // Declare a variable
+  val greeting: String = "Hello, World!"
+
+  // Print the variable
+  println(greeting)
+}
